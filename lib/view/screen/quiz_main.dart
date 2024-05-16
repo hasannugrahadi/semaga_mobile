a3c5d2ec1e05aad773c6271828d399d470bc140a
hasannugrahadi <hknx78@gmail.com>
2024-05-16 22:29:06 +0700

    Dashboard update, History & Profile in progress

--- lib/view/screen/quiz_main.dart
+++ lib/view/screen/quiz_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuizMain extends StatefulWidget {
   const QuizMain({super.key});
@@ -11,10 +12,28 @@ class _QuizMainState extends State<QuizMain> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-
-      ),
-      body:
+        appBar: AppBar(
+          leading: Builder(
+            builder: (context) => IconButton(
+                icon: const Icon(Icons.menu), iconSize: 30,
+                color: Colors.white,
+                onPressed: () {
+                  Scaffold.of(context).openDrawer();
+                }
+            ),
+          ),
+          title: Text("Belum Selesai",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.normal,
+              fontSize: 24,
+              fontFamily: GoogleFonts.robotoCondensed().fontFamily,
+            ),
+          ),
+          titleSpacing: 0.0,
+          backgroundColor: const Color(0xff4682A9),
+        ),
+      body: const Placeholder()
     );
   }
 }
